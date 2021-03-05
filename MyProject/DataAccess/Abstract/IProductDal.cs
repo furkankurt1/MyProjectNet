87437f3c1b18c7190864a1832436022f83bcac25
furkan <furkankurt@msn.com>
2021-03-05 23:27:05 +0000

    Example values are assigned and functions has been created.

---
+++ MyProject/DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+
+    public interface IProductDal 
+    {
+
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+    }
+
+}
---
+++ MyProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+
+    public class InMemoryProductDal : IProductDal
+    {
+
+        private List<Product> _products;
+
+        public InMemoryProductDal(){
+            _products = new List<Product>()
+            {
+                new Product() {CategoryId = 1, ProductId = 1, ProductName = "Bardak", UnitsInStock = 150, UnitPrice = 100},
+                new Product() {CategoryId = 1, ProductId = 2, ProductName = "Kamera", UnitsInStock = 10, UnitPrice = 100},
+                new Product() {CategoryId = 2, ProductId = 3, ProductName = "At", UnitsInStock = 5, UnitPrice = 75},
+                new Product() {CategoryId = 2, ProductId = 4, ProductName = "Eşek", UnitsInStock = 2, UnitPrice = 1000},
+                new Product() {CategoryId = 3, ProductId = 5, ProductName = "Katır", UnitsInStock = 15, UnitPrice = 50}
+            };
+        }
+
+        public List<Product> GetAll(){
+            return _products;
+        }
+
+        public void Add(Product product){
+            _products.Add(product);
+        }
+
+        public void Update(Product product){
+            throw new System.NotImplementedException();
+        }
+
+        //2.10 7.d
+        public void Delete(Product product){
+            _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+        }
+
+    }
+
+}
