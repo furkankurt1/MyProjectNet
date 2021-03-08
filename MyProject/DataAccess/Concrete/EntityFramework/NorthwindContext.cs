54b9e5331fd5abe456223bc791d77197b553ed54
furkan <furkankurt@msn.com>
2021-03-08 17:54:11 +0100

    db is connected.

--- MyProject/DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ MyProject/DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -5,15 +5,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class NorthwindContext : DbContext
+    public class NorthwindContext :DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; 
-database=northwind;trusted_connection=true;User Id=furkan; password =123456");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;database=northwind;
+                                    trusted_connection=true");
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
     }
+
 }
